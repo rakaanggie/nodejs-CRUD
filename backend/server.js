233dc1d1d324571d289d4db9364e9dd2dd7b3ff0
rakaanggie <rakaanggie123@gmail.com>
2023-07-01 18:40:09 +0700

    update backend environtment and add some features

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql");
+require('dotenv').config();
 
 const app = express();
-
+app.use(express.json());
 app.use(cors());
 
 const db = mysql.createConnection({
-    host: "application.clqqyhrtzbj3.us-east-1.rds.amazonaws.com",
-    user: "admin",
-    password: "12345678",
-    database: "application"
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 })
 
 app.get("/check", (req, res) => {
@@ -21,6 +22,40 @@ app.get("/", (req, res) => {
     const sql = "SELECT * FROM `student`";
     db.query(sql, (err, data) => {
         if(err) return res.json(ERROR);
+        return res.json(data);
+    })
+})
+
+app.post ("/create", (req, res) => {
+    const sql = "INSERT INTO `student` (Name, Email) VALUES (?)";
+    const values = [
+        req.body.name,
+        req.body.email
+    ]
+    db.query(sql, [values], (err, data) => {
+        if(err) return res.json("Error");
+        return res.json(data);
+    })
+})
+
+app.put ("/update/:id", (req, res) => {
+    const sql = "UPDATE `student` set `Name` = ?, `Email` = ? where ID = ?";
+    const values = [
+        req.body.name,
+        req.body.email
+    ]
+    const id = req.params.id;
+    db.query(sql, [...values, id], (err, data) => {
+        if(err) return res.json("Error");
+        return res.json(data);
+    })
+})
+
+app.delete ("/student/:id", (req, res) => {
+    const sql = "DELETE FROM `student` WHERE ID = ?";
+    const id = req.params.id;
+    db.query(sql, [id], (err, data) => {
+        if(err) return res.json("Error");
         return res.json(data);
     })
 })
